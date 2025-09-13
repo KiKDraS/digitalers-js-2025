bcab0806e7718c9c5d7e1f86368604e93e11a43f
Anahí Foresi <anahiforesi@gmail.com>
2025-09-13 13:12:54 -0300

    inicio clase-30

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const start = () => {
-  // asincronia();
-  // apiWebFetch();
-  asyncAwait();
-};
-
-// Agregamos la escucha del DOMContentLoaded para ejecutar todo nuestro JS después de que se carga el DOM
-document.addEventListener("DOMContentLoaded", start);
