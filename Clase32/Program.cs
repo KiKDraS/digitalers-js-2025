7c306fa9b05b7e4aefeefbf210d78530b2ecdab3
Anahí Foresi <anahiforesi@gmail.com>
2025-09-20 09:17:12 -0300

    Inicio

---
+++ Clase32/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Clase32;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            Paradigma de Programación Orientada a Objetos        
+                -> 4 Pilares
+                    -> Abstracción - Se utilizan para pensar qué capacidades tiene que tener una Clase
+                    -> Herencia - Se utiliza para no repetir código innecesario
+                    -> Encapsulamiento - Una Clase no debe permitir modificaciones por fuera de la misma
+                        -> Modificadores de visibilidad
+                            -> public - Cualquiera puede usar el dato
+                            -> private - Solo la Clase puede utilizar el dato
+                            -> protected - Sola la Clase y sus Clases herederas pueden usar/modificar el valor
+                        -> Getters - Métodos public que permiten que otras Clases lean el valor almacenado en la propiedad private
+                        -> Setters - Métodos public que permiten que otras Clases soliciten la modificación de un valor private  
+                    -> Polimorfismo
+        */
+    }
+}
